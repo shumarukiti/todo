61ec2b259d19e915b1fb76c2bb1cba3a93c1743d
Shujiro Kanaya <j1info.com@gmail.com>
2020-12-29 13:18:10 +0900

    annotate導入して適応

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  overview   :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_boards_on_user_id  (user_id)
+#
+
 class Board < ApplicationRecord
 end
