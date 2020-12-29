048247f7852480cbf1381d70d8f66e9172f2d62f
Shujiro Kanaya <j1info.com@gmail.com>
2020-12-29 17:42:36 +0900

    pry-byebug導入、ボード作成時にデータが渡らない不具合解消

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -11,12 +11,17 @@ class BoardsController < ApplicationController
     end
 
     def create
-      @board = current_user.boards.build
+      @board = current_user.boards.build(board_params)
       if @board.save
         redirect_to board_path(@board)
       else
         flash.now[:errow] = '保存に失敗しました'
         render new
       end
+    end
+
+    private
+    def board_params
+      params.require(:board).permit(:name, :overview)
     end
 end
