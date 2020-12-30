970834b144f638abeaac2359396d9c2186e2dff0
Shujiro Kanaya <j1info.com@gmail.com>
2020-12-30 16:28:04 +0900

    タスクの編集機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,9 +15,22 @@ class TasksController < ApplicationController
         redirect_to board_path(params[:board_id])
     end
 
+    def edit
+        @task = Task.find(params[:id])
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            redirect_to board_path(params[:board_id])
+        else
+            flash.noe[:error] = ''
+        end
+    end
+
     private
         def task_params
-            params.permit(
+            params.require(:task).permit(
                 :content,
                 :deadline,
                 :title)
